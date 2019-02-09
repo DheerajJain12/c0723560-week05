901f4143624c2d1090a0478f5f752b5e0b96e5aa
Dheeraj Jain <c0723560@mylambton.ca>
2019-02-08 21:13:43 -0500

    final commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,89 +8,88 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        class Program
         {
-            Elevator e = new Elevator();
-            e.setup();
-            e.TraverseList();
-        }
-    }
-
-    class Node
-    {
-        public Node() { }
-        public Node elevatorUp;
-        public string FloorNumber;
-    }
-
-    class Department
-    {
-        public Department(string dept_name)
-        {
-            DepartmentDescription = dept_name;
-        }
-        public Department nextDepartment;
-        public Department previousDepartment;
-        public string DepartmentDescription;
-    }
-
-    class DepartmentStore
-    {
-        public void InitializeDepartments()
-        {
-            Department Books = new Department("Books");
-            Department Kitchenware = new Department("Kitchenware");
-
-            Kitchenware.nextDepartment = Books;
-            Kitchenware.previousDepartment = FirstFloor;
-            Department Books = new Department("Books");
-
-        }
-
-    }
-
-    class Elevator
-    {
-        public static Node Head;
-        public static Node FirstFloor;
-        public static Node SecondFloor;
-        public static Node ThirdFloor;
-        public static Node FourthFloor;
-
-
-
-
-        public void setup()
-        {
-            FirstFloor = new Node();
-            SecondFloor = new Node();
-            ThirdFloor = new Node();
-            FourthFloor = new Node();
-            Head = FirstFloor;
-            FirstFloor.FloorNumber = "First Floor";
-            FirstFloor.elevatorUp = SecondFloor;
-            SecondFloor.FloorNumber = "Second Floor";
-            SecondFloor.elevatorUp = ThirdFloor;
-            ThirdFloor.FloorNumber = "Third Floor";
-            ThirdFloor.elevatorUp = FourthFloor;
-            FourthFloor.FloorNumber = "Fourth Floor";
-            FourthFloor.elevatorUp = null;
-        }
-
-        public void TraverseList()
-        {
-            Node temp;
-            temp = Head;
-
-            // where am I going to start?
-
-            while (temp != null)
+            static void Main(string[] args)
             {
-                Console.WriteLine(temp.FloorNumber);
-                temp = temp.elevatorUp;
+                Elevator e = new Elevator();
+                e.setup();
+                e.TraverseList();
+            }
+        }
+
+        class Node
+        {
+            public Node() { }
+            public Node elevatorUp;
+            public string FloorNumber;
+        }
+
+        class Department
+        {
+            public Department(string dept_name)
+            {
+                DepartmentDescription = dept_name;
+            }
+            public Department nextDepartment;
+            public Department previousDepartment;
+            public string DepartmentDescription;
+        }
+
+        class DepartmentStore
+        {
+            public void InitializeDepartments()
+            {
+                Department Books = new Department("Books");
+                Department Kitchenware = new Department("Kitchenware");
+
+                Kitchenware.nextDepartment = Books;
+                Kitchenware.previousDepartment = FirstFloor;
+                Department Books = new Department("Books");
 
             }
 
         }
+
+        class Elevator
+        {
+            public static Node Head;
+            public static Node FirstFloor;
+            public static Node SecondFloor;
+            public static Node ThirdFloor;
+            public static Node FourthFloor;
+
+            public void setup()
+            {
+                FirstFloor = new Node();
+                SecondFloor = new Node();
+                ThirdFloor = new Node();
+                FourthFloor = new Node();
+                Head = FirstFloor;
+                FirstFloor.FloorNumber = "First Floor";
+                FirstFloor.elevatorUp = SecondFloor;
+                SecondFloor.FloorNumber = "Second Floor";
+                SecondFloor.elevatorUp = ThirdFloor;
+                ThirdFloor.FloorNumber = "Third Floor";
+                ThirdFloor.elevatorUp = FourthFloor;
+                FourthFloor.FloorNumber = "Fourth Floor";
+                FourthFloor.elevatorUp = null;
+            }
+
+            public void TraverseList()
+            {
+                Node temp;
+                temp = Head;
+
+                // where am I going to start?
+
+                while (temp != null)
+                {
+                    Console.WriteLine(temp.FloorNumber);
+                    temp = temp.elevatorUp;
+
+                }
+
+            }
+        }
     }
-}
